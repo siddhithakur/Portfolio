891557b777a51cbd940cecf076f355545aa6931f
sidythakur <siddhithakur56@gmail.com>
2020-03-30 19:33:13 +0530

    local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 MEDIA_URL='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like you are on production")
